f3640174876af3c5e36289a16ea8470664a68566
Ajay Kamble <ajay@technogise.com>
2019-02-11 23:11:54 +0530

    created a generic fn to test divisibility, tests still passing

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,8 +1,11 @@
 (ns fizzbuzz.core)
 
+(defn divisible-by? [n & nums]
+  (reduce (fn [acc result] (and acc (zero? (rem n result)))) true nums))
+
 (defn generate [n]
   (cond
-    (and (zero? (rem n 3)) (zero? (rem n 5))) "FizzBuzz"
-    (zero? (rem n 3)) "Fizz"
-    (zero? (rem n 5)) "Buzz"
+    (divisible-by? n 3 5) "FizzBuzz"
+    (divisible-by? n 3) "Fizz"
+    (divisible-by? n 5) "Buzz"
     :else (str n)))
