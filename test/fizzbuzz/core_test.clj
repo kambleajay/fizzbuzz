7458f8496129f9a9f77a1c3b12e2d35d8f0fce8b
Ajay Kamble <ajay@technogise.com>
2019-02-11 16:46:51 +0530

    removed sample test and added first failing test

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -2,6 +2,5 @@
   (:require [clojure.test :refer :all]
             [fizzbuzz.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest generate-test
+  (is (= 3 (generate 3))))
