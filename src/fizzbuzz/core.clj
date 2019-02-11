a6d104737c50b0fa5e013739a510a41c48d11a64
Ajay Kamble <ajay@technogise.com>
2019-02-11 22:38:25 +0530

    generated correct generators for multiples of 3 (but not of 5), multiples of 5 (but not of 3) and multiples of 15

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -2,5 +2,6 @@
 
 (defn generate [n]
   (cond
+    (and (zero? (rem n 3)) (zero? (rem n 5))) "FizzBuzz"
     (zero? (rem n 3)) "Fizz"
     (zero? (rem n 5)) "Buzz"))
--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -5,19 +5,33 @@
             [clojure.test.check.properties :as prop]
             [clojure.test.check.clojure-test :refer [defspec]]))
 
-(defn multiples-of-n [n]
-  (->> gen/int
-       (gen/fmap #(* % n))
-       (gen/such-that #(not= % 0))))
+(defn multiple-of-n [n]
+  (fn [x] (zero? (rem x n))))
+
+(def multiple-of-3 (multiple-of-n 3))
+(def multiple-of-5 (multiple-of-n 5))
+(def multiple-of-15 (multiple-of-n 15))
+
+(def not-multiple-of-3 (complement multiple-of-3))
+(def not-multiple-of-5 (complement multiple-of-5))
+
+(def multiples-of-3
+  (gen/fmap #(* % 3) (gen/such-that #(and (not-multiple-of-5 %) (not= % 0)) gen/int 100)))
+
+(def multiples-of-5
+  (gen/fmap #(* % 5) (gen/such-that #(and (not-multiple-of-3 %) (not= % 0)) gen/int 100)))
+
+(def multiples-of-15
+  (gen/such-that #(and (multiple-of-15 %) (not= % 0)) gen/int 100))
 
 (defspec generate-multiples-of-3-test
-  (prop/for-all [n (multiples-of-n 3)]
+  (prop/for-all [n multiples-of-3]
                 (= (generate n) "Fizz")))
 
 (defspec generate-multiples-of-5-test
-         (prop/for-all [n (multiples-of-n 5)]
-                       (= (generate n) "Buzz")))
+  (prop/for-all [n multiples-of-5]
+                (= (generate n) "Buzz")))
 
 (defspec generate-multiples-of-3-and-5-test
-         (prop/for-all [n (multiples-of-n 15)]
-                       (= (generate n) "FizzBuzz")))
+  (prop/for-all [n multiples-of-15]
+                (= (generate n) "FizzBuzz")))
