497c19969dad97b20d25146a3195c937aaffc599
Ajay Kamble <ajay@technogise.com>
2019-02-11 17:04:46 +0530

    added test.check to have generative testing, currently passing as Fizz is hardcoded

--- test/fizzbuzz/core_test.clj
+++ test/fizzbuzz/core_test.clj
@@ -1,6 +1,14 @@
 (ns fizzbuzz.core-test
   (:require [clojure.test :refer :all]
-            [fizzbuzz.core :refer :all]))
+            [fizzbuzz.core :refer :all]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]))
 
-(deftest generate-test
-  (is (= "Fizz" (generate 3))))
+(def multiples-of-3 (->> gen/int
+                         (gen/fmap #(* % 3))
+                         (gen/such-that #(not= % 0))))
+
+(defspec generate-multiples-of-3-test
+  (prop/for-all [n multiples-of-3]
+                (= (generate n) "Fizz")))
